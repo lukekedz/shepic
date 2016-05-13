85f42d73b9d6732834efc3b708f4910701c45056
LukeKedz <lukekedziora@gmail.com>
2016-05-12 20:27:07 -0400

    implementing simple form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.create!(email: "lk@gmail.com", password: "loplop", password_confirmation: "loplop")
+User.create!(email: "luke@gmail.com", password: "loplop", password_confirmation: "loplop")
