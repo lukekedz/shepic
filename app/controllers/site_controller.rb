864d32dcbbda0f152e88a48b9c4ea93b55fdec7f
LukeKedz <lukekedziora@gmail.com>
2016-05-13 19:49:22 -0400

    layout in progress; viewing in FF mobile-view

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,4 +5,10 @@ class SiteController < ApplicationController
   def welcome
   end
 
+  def admin_add_for_week
+    @games = [[:away => "Florida",   :home => "Florida State",:spread => "+1", :dawg => "-1",:location => "Jacksonville, FL"],
+              [:away => "Penn State",:home => "Ohio State",   :spread => "-15",:dawg => "+15",:location => "Columbus, OH"    ],
+              [:away => "Kentucky",  :home => "Alabama",      :spread =>  "-23",:dawg => "+23",:location => "Missouri, MS"    ]]
+  end
+
 end
