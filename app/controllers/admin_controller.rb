09553b0117ac53225e03ff7a0e219b9a62e27e08
LukeKedz <lukekedziora@gmail.com>
2016-05-15 17:59:14 -0400

    basic admin add game working

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,26 @@
 class AdminController < ApplicationController
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
   before_action :user_is_admin?
 
   def current_week
     @current_week = Week.last
     @games = Game.where(week_id: @current_week.id)
+    @new_game = Game.new
   end
 
   def new_week
+
+  end
+
+  def add_new_game
+    # puts params
+    @new_game = Game.new(game_params)
+
+    if @new_game.save
+      redirect_to admin_current_week_path
+    end
   end
 
   private
@@ -17,6 +29,10 @@ class AdminController < ApplicationController
     unless current_user.admin
       redirect_to root_path
     end
+  end
+
+  def game_params
+    params.require(:game).permit(:week_id, :away, :home, :spread, :location, :tiebreaker)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'site#welcome'
   get  'admin/current_week'
+  post 'admin/add_new_game'
 
   devise_for :users
 
