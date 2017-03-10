b1a6fb31040a3d733da62bc8567456c7a27a0340
LukeKedz <lukekedziora@gmail.com>
2017-03-10 11:45:00 -0500

    added 2nd seed file for simulation

---
+++ db/simulation/user_picks.rb
@@ -0,0 +1,21 @@
+current_week_id = Week.last.id
+games = Game.where(week_id: current_week_id)
+
+User.all.each do |user|
+
+	games.each do |game|
+		away_home = ["away", "home"]
+		versus    = [game.away, game.home]
+	    selector  = rand(0..1)
+	    tbreak    = game.tiebreaker == true ? rand(0..160) : "" 
+
+		Pick.where(user_id: user.id, game_id: game.id)
+			.first_or_create(user_id:    user.id,
+							 game_id:    game.id,
+							 away_home:  away_home[selector],
+							 pick:       versus[selector],
+							 tbreak_pts: tbreak
+							)
+	end
+
+end
