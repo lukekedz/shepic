b9d1f695a9b52979904b30b5d40c6af43671fa17
LukeKedz <lukekedziora@gmail.com>
2017-03-27 11:01:14 -0400

    removing bash script; converting to Rake task

--- rakescript.sh
+++
@@ -1,6 +0,0 @@
-# SOURCE http://www.maclife.com/article/columns/terminal_101_automate_terminal_bash_scripts
-# $> bundle exec bash rakescript.sh
-rake db:drop
-rake db:create
-rake db:migrate
-rake db:seed
