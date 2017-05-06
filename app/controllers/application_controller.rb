3fcaa53b27246b536e1d71fe732623ef267a442e
LukeKedz <lukekedziora@gmail.com>
2017-05-06 08:32:25 -0400

    improved UX for history; new User has username, auto set to admin=false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  
+
   # :nocov:
   before_action do |controller|
     @current_week = Week.current()
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
+	after_create :default_admin
 	after_create :set_standing_wins
 
 	has_many :picks
 	has_one  :standing
+
+	def default_admin
+		user = User.last
+	   	user.admin = false
+	   	user.save
+	end
 
 	def set_standing_wins
 		user = User.last
@@ -12,4 +19,5 @@ class User < ActiveRecord::Base
 			Standing.create(user_id: user.id, wins: 0)
 		end
 	end
+
 end
