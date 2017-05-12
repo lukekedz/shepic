af5cebf4de8d27bca2027ebca77da58ecd0e0f78
LukeKedz <lukekedziora@gmail.com>
2017-05-12 14:20:32 -0400

    upd spacing

--- db/simulation/ten_game_slate.rb
+++ db/simulation/ten_game_slate.rb
@@ -2,14 +2,14 @@ start_times = ["1200", "1215", "300", "330", "800", "830", "900"]
 
 10.times do
   Game.create(
-    week_id:    Week.last.week,
-    away:       Team.all.sample.name,
-    home:       Team.all.sample.name,
-    spread:     Faker::Number.between(-15, 15),
-    location:   Faker::Address.city,
-    tiebreaker: false,
-    date:       Faker::Date.between(Date.today, Date.today + 2),
-    start_time: start_times.sample,
+    week_id:      Week.last.week,
+    away:         Team.all.sample.name,
+    home:         Team.all.sample.name,
+    spread:       Faker::Number.between(-15, 15),
+    location:     Faker::Address.city,
+    tiebreaker:   false,
+    date:         Faker::Date.between(Date.today, Date.today + 2),
+    start_time:   start_times.sample,
     game_started: false
   )
 end
