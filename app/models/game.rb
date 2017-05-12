697c68c36f61ef98e0e87a75c1ce22719103e48c
LukeKedz <lukekedziora@gmail.com>
2017-05-12 14:18:44 -0400

    switched game.rb times[] to military time

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,20 +2,19 @@ class Game < ActiveRecord::Base
   validates :week_id,     presence: true
   validates :away,        presence: true
   validates :home,        presence: true
-  validates_inclusion_of :tiebreaker, in: [true, false]
+  validates_inclusion_of :tiebreaker,   in: [true, false]
+  validates_inclusion_of :game_started, in: [true, false]
 
   belongs_to :weeks
   has_many :picks
 
   def game_times
     times = []
-    (1..12).each do |i|
-      times.push(i.to_s + ":00" + "pm")
-      times.push(i.to_s + ":15" + "pm")
-      times.push(i.to_s + ":30" + "pm")
+    (12..23).each do |i|
+      times.push(i.to_s + "00")
+      times.push(i.to_s + "15")
+      times.push(i.to_s + "30")
     end
-
-    3.times { times.insert(0, times.delete_at(-1)) }
 
     times
   end
