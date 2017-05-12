a4414d968d968058211a4418de04b75db1a51ec4
LukeKedz <lukekedziora@gmail.com>
2017-05-12 14:33:40 -0400

    abstracted convert military time to reg ampm for btr display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,21 @@
 module ApplicationHelper
 
-  def game_day(date)
-    date.strftime("%A, %b %e")
-  end
+    def game_day(date)
+        date.strftime("%A, %b %e")
+    end
+
+    def convert_military_to_ampm(military_time_str)
+        ampm_time = ""
+
+        if military_time_str[0..1] == "12"
+            ampm_time = military_time_str[0..1]
+        else
+            ampm_time = (military_time_str[0..1].to_i - 12).to_s
+        end
+
+        ampm_time += ":" + military_time_str[2..3] + "pm"
+
+        ampm_time
+    end
 
 end
