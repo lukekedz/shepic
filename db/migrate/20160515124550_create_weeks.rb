1469ebb555126f13af19c8b6d95a07955994ec92
LukeKedz <lukekedziora@gmail.com>
2017-06-07 13:45:04 -0400

    Week.new has default false for locked, finalized

--- db/migrate/20160515124550_create_weeks.rb
+++ db/migrate/20160515124550_create_weeks.rb
@@ -1,10 +1,10 @@
 class CreateWeeks < ActiveRecord::Migration
   def change
     create_table :weeks do |t|
-      t.integer :week, null: false
-      t.boolean :locked
-      t.boolean :finalized
-
+      t.integer :week,      null: false
+      t.boolean :locked,    default: false
+      t.boolean :finalized, default: false
+      
       t.timestamps null: false
     end
   end
