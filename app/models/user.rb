8bc773dc52a765b2cff20f592a69aea193b91f52
LukeKedz <lukekedziora@gmail.com>
2017-08-12 11:54:59 -0400

    schema upd user default admin is false; upd seeds and user model to reflect

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
-	after_create :default_admin
 	after_create :set_standing_wins
 
 	has_many :picks
 	has_one  :standing
-
-	# TODO: prevent new Users from being Admin
-	# this causes my development seed Admin user to flip to a regular user
-	def default_admin
-		user = User.last
-	   	user.admin = false
-	   	user.save
-	end
 
 	def set_standing_wins
 		user = User.last
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,16 @@
+puts
 puts "Seeding DB: Initial..."
+puts
 
-# initial setup
-# TODO: add .env variable for dev instance and improve performance here
-# i.e., create an admin account, but do not add to Standings
-if User.count == 0
-  User.create!(admin: true,  username: "luke",  email: "luke@gmail.com",  password: "loplop", password_confirmation: "loplop" )
-  User.create!(admin: false, username: "kr",    email: "kr@gmail.com",    password: "emmie1", password_confirmation: "emmie1" )
-  User.create!(admin: false, username: "lk",    email: "lk@gmail.com",    password: "emmie2", password_confirmation: "emmie2" )
-  User.create!(admin: false, username: "gary",  email: "gary@gmail.com",  password: "kickba", password_confirmation: "kickba" )
-  User.create!(admin: false, username: "kat",   email: "kat@gmail.com",   password: "emmie2", password_confirmation: "emmie2" )
-  User.create!(admin: false, username: "emmie", email: "emmie@gmail.com", password: "hooman", password_confirmation: "hooman" )
-end
+User.create!(username: "luke",  email: "luke@gmail.com",  password: "loplop", password_confirmation: "loplop", admin: true)
+User.create!(username: "kr",    email: "kr@gmail.com",    password: "emmie1", password_confirmation: "emmie1")
+User.create!(username: "lk",    email: "lk@gmail.com",    password: "emmie2", password_confirmation: "emmie2")
+User.create!(username: "gary",  email: "gary@gmail.com",  password: "kickba", password_confirmation: "kickba")
+User.create!(username: "kat",   email: "kat@gmail.com",   password: "emmie2", password_confirmation: "emmie2")
+User.create!(username: "emmie", email: "emmie@gmail.com", password: "hooman", password_confirmation: "hooman")
 
 teams = ["Air Force",  "Akron",  "Alabama",  "Arizona", "Army", "Auburn", "Ball State", "Baylor", "BYU", "Boise State", "Boston College", "Buffalo", "Cal", "UCF", "Central Michigan", "Cincinnati", "Clemson", "Colorado", "Colorado State", "UConn", "Duke", "Eastern Michigan", "Florida", "Florida Atlantic", "Florida International", "Florida State", "Fresno State", "Georgia", "Georgia Tech", "Hawaii", "Houston", "Idaho", "Illinois", "Indiana", "Iowa", "Iowa State", "Kansas", "Kansas State", "Kent State", "Kentucky", "LSU", "Louisiana Tech", "Louisiana-Lafayette", "Louisiana-Monroe", "Louisville", "Marshall", "Maryland", "Memphis", "Miami (FL)", "Miami (OH)", "Michigan", "Michigan State", "Middle Tennessee State", "Minnesota", "Mississippi", "Mississippi State", "Missouri", "Navy", "Nebraska", "Nevada", "UNLV", "New Mexico", "New Mexico State", "North Carolina", "NC State", "North Texas", "Northern Illinois", "Northwestern", "Notre Dame", "Ohio", "Ohio State", "Oklahoma", "Oklahoma State", "Oregon", "Oregon State", "Penn State", "Pitt", "Purdue", "Rice", "Rutgers", "San Diego State", "San Jose State", "South Carolina", "South Florida", "SMU", "Southern Mississippi", "Stanford", "Syracuse", "Temple", "Tennessee", "Texas", "Texas A&M", "TCU", "Texas Tech", "Texas-El Paso", "Toledo", "Troy", "Tulane", "Tulsa", "UCLA", "Utah", "Utah State", "Vanderbilt", "Virginia", "Virginia Tech", "Wake Forest", "Washington", "Washington State", "West Virginia", "Western Kentucky", "Western Michigan", "Wisconsin", "Wyoming"]
 
-if Team.count == 0 
-  teams.each { |t| Team.create(name: t) }
-end
+teams.each { |t| Team.create(name: t) }
 
 Week.create(week: 1, locked: false, finalized: false)
