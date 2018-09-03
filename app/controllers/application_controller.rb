1dcff5817c3871f391ab8acd79540e1b32692aca
LukeKedz <lukekedziora@gmail.com>
2018-09-02 21:27:49 -0400

    new post route does not need to authorize user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:active_game_slate, :game_started]
+  before_action :authenticate_user!, except: [:active_game_slate, :game_started, :update_score]
 
   # :nocov:
   before_action do |controller|
